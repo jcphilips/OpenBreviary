4e62efaebbb618357a6647080ea127b112ac20d8
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2026-02-10 21:03:03 +0530

    test(Core): Create initial tests for calculator logic

---
+++ tests/OpenBreviary.Tests/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using OpenBreviary.Core;
+
+namespace OpenBreviary.Tests;
+
+public class CalculatorTests
+{
+  [Fact]
+  public void ReturnsFifthSundayInOrdinaryTime()
+  {
+    // Arrange
+    var calculator = new LiturgicalCalculator();
+    var sundayInOrdinaryTime = new Date(2026, 2, 9);
+
+    // Act
+    var result = calculator.CalculateDay(sundayInOrdinaryTime);
+
+    // Assert
+    Assert.Equal("5th Sunday in Ordinary Time", result);
+  }
+}
