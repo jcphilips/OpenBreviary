66170e9d9a36641e35974e5f8b6fb2e9204b6c07
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2026-02-15 23:22:41 +0530

    tests(CalculatorTests): Maintain TDD for Calculator testing

--- tests/OpenBreviary.Tests/CalculatorTests.cs
+++ tests/OpenBreviary.Tests/CalculatorTests.cs
@@ -4,17 +4,36 @@ namespace OpenBreviary.Tests;
 
 public class CalculatorTests
 {
-  [Fact]
-  public void ReturnsFifthSundayInOrdinaryTime()
+  [Theory]
+  [InlineData(2024, "2024-03-31")]
+  [InlineData(2025, "2025-04-20")]
+  [InlineData(2026, "2026-04-05")]
+  public void ReturnsEasterDay(int year, string actualDate)
   {
     // Arrange
+    var easterDay = DateTime.Parse(actualDate);
     var calculator = new LiturgicalCalculator();
-    var sundayInOrdinaryTime = new Date(2026, 2, 9);
 
     // Act
-    var result = calculator.CalculateDay(sundayInOrdinaryTime);
+    var result = calculator.CalculateEaster(year);
 
     // Assert
-    Assert.Equal("5th Sunday in Ordinary Time", result);
+    Assert.Equal(easterDay, result);
   }
+
+  [Theory]
+  [InlineData(2026, "2025-11-30")]
+  [InlineData(2030, "2029-12-02")]
+  [InlineData(2021, "2020-11-29")]
+  public void ReturnsFirstSundayOfAdvent(int year, string actualDate)
+  {
+    var calculator = new LiturgicalCalculator();
+    var expected = DateTime.Parse(actualDate);
+
+    var result = calculator.FirstSundayOfAdvent(year);
+
+    // Assert
+    Assert.Equal(expected, result);
+  }
+
 }
