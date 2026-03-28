282102d8974aca928a04aef6de888fbe4eab4747
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2026-03-28 16:08:04 +0530

    tests(OfficeService): create tests for office service functionality

--- tests/OpenBreviary.Tests/OfficeServiceTests.cs
+++ tests/OpenBreviary.Tests/OfficeServiceTests.cs
@@ -1,13 +1,19 @@
 using OpenBreviary.Core;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace OpenBreviary.Tests
 {
   public class OfficeServiceTests
   {
+
     [Theory]
     [InlineData("2010-12-25", LiturgicalSeason.Christmastide)]
     [InlineData("2026-02-18", LiturgicalSeason.Lent)]
-    public void ReturnsLiturgicalDay(string date, LiturgicalSeason expected)
+    [InlineData("2026-02-17", LiturgicalSeason.OrdinaryTime)]
+    [InlineData("2026-02-25", LiturgicalSeason.Lent)]
+    [InlineData("2026-04-05", LiturgicalSeason.EasterOctave)]
+    [InlineData("2026-08-05", LiturgicalSeason.OrdinaryTime)]
+    public void ReturnCorrectLiturgicalSeason(string date, LiturgicalSeason expected)
     {
       var calculator = new LiturgicalCalculator();
       var service = new OfficeService(calculator);
@@ -25,7 +31,7 @@ namespace OpenBreviary.Tests
       var services = new ServiceCollection();
       services.AddSingleton<LiturgicalCalculator>();
       services.AddTransient<OfficeService>();
-      var provider = services.BuldServiceProvider();
+      var provider = services.BuildServiceProvider();
 
       var service = provider.GetRequiredService<OfficeService>();
 
