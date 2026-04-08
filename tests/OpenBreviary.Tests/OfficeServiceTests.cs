d951020f67e8797cb2589909c6221cef73e9ce16
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2026-04-08 20:52:02 +0530

    tests(OfficeService): formatting and some other corrections

--- tests/OpenBreviary.Tests/OfficeServiceTests.cs
+++ tests/OpenBreviary.Tests/OfficeServiceTests.cs
@@ -6,6 +6,20 @@ namespace OpenBreviary.Tests
   public class OfficeServiceTests
   {
 
+    [Fact]
+    public void DI_Container_resolves_OfficeService()
+    {
+      //Given
+      var services = new ServiceCollection();
+      services.AddSingleton<LiturgicalCalculator>();
+      services.AddTransient<OfficeService>();
+      var provider = services.BuildServiceProvider();
+
+      var service = provider.GetRequiredService<OfficeService>();
+
+      Assert.NotNull(service);
+    }
+
     [Theory]
     [InlineData("2010-12-25", LiturgicalSeason.Christmastide)]
     [InlineData("2026-02-18", LiturgicalSeason.Lent)]
@@ -26,18 +40,38 @@ namespace OpenBreviary.Tests
       Assert.Equivalent(expected, result);
     }
 
-    [Fact]
-    public void DI_Container_resolves_OfficeService()
+    [Theory]
+    [InlineData("2010-12-25", 1)]
+    [InlineData("2026-02-18", 0)]
+    [InlineData("2026-02-17", 6)]
+    [InlineData("2026-02-25", 1)]
+    [InlineData("2026-03-29", 6)]
+    [InlineData("2026-04-03", 6)]
+    [InlineData("2026-04-05", 1)]
+    public void ReturnsWeekOfSeason(string date, int expectWeekOfSeason)
     {
-      //Given
-      var services = new ServiceCollection();
-      services.AddSingleton<LiturgicalCalculator>();
-      services.AddTransient<OfficeService>();
-      var provider = services.BuildServiceProvider();
+      var calculator = new LiturgicalCalculator();
+      var service = new OfficeService(calculator);
+      var ds = DateTime.Parse(date);
 
-      var service = provider.GetRequiredService<OfficeService>();
+      var result = service.GetLiturgicalDay(ds).WeekOfSeason;
 
-      Assert.NotNull(service);
+      Assert.Equal(expectWeekOfSeason, result);
+    }
+
+
+    [Theory]
+    [InlineData("2026-03-29", 2)]
+    [InlineData("2026-03-22", 1)]
+    public void ReturnsCorrectPsalterWeek(string date, int expectedPsalterWeek)
+    {
+      var calculator = new LiturgicalCalculator();
+      var service = new OfficeService(calculator);
+      var ds = DateTime.Parse(date);
+
+      var result = service.GetLiturgicalDay(ds).PsalterWeek;
+
+      Assert.Equal(expectedPsalterWeek, result);
     }
   }
 }
